f46ceccf33b68cb1c7ca4b8d81465d64203095a7
pietrodub <pietroti453@gmail.com>
2026-04-03 19:47:20 -0300

    aaaaaaaaaaa

--- Python_estruturas/Array/last_word.py
+++ Python_estruturas/Array/last_word.py
@@ -1,2 +1,6 @@
-word = "  a             "
-print("a" + word.strip() + "a")
+# minha solução
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        string = s.strip() #trim
+        string = s.split() # tira todos os espacos " ", "  "
+        return len(string[-1])
