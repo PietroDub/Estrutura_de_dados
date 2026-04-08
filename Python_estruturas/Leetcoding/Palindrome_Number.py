ccdb6eb179daccca072e73a7e5a468177d6f0998
pietrodub <pietroti453@gmail.com>
2026-04-07 21:56:54 -0300

    palíndrome

---
+++ Python_estruturas/Leetcoding/Palindrome_Number.py
@@ -0,0 +1,23 @@
+
+#testes
+
+#The original array
+arr = [11, 22, 33, 44, 55]
+x= arr
+print("Before reversal Array is :",arr)
+ 
+arr.reverse() #reversing using reverse()
+print("After reversing Array:",arr)
+
+if(arr == x):
+    print("VDD")
+else:
+    print("NOPE")
+
+#resolução
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = list(str(x))
+        a = x[:]  # ← MUDE SÓ ISSO!
+        x.reverse()
+        return x == a
